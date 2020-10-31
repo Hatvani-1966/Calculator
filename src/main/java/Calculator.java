9351e337fa7ae78b1a14c8ad1dad531ae02e3cbb
Hatvani-1966 <hatvaniattila@t-online.hu>
2020-10-31 17:59:24 +0100

    firs version A

---
+++ src/main/java/Calculator.java
@@ -0,0 +1,19 @@
+import java.util.Scanner;
+
+public class Calculator {
+  public static void main (String[] args) {
+      System.out.println("Calculator");
+      String message="Calculator;";
+              System.out.println(message);
+      System.out.println("Give two int number");
+      Scanner scanner = new Scanner(System.in);
+      String name = scanner.nextLine();
+      System.out.println("Numbers"+name);
+      System.out.println("Int number");
+      int number = scanner.nextInt();
+      System.out.println("Int number"+ number);
+
+      System.out.println(name);
+  }
+
+}
